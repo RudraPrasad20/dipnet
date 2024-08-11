0bf83e74bf0adfc8739ad32f2f72335603f6e0f4
Rudra Prasad <rudraofficial202@gmail.com>
2024-08-11 08:28:49 +0530

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,18 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}><Navbar/>{children}</body>
+        <body className={inter.className}>
+          {" "}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "./mode-toggle";
 
 const Navbar = () => {
   return (
-    <div className=" h-16 bg-slate-200">
-      <div className="flex justify-between items-center align-middle p-3">
-        <h1 className=" font-extrabold text-2xl">DIPNET</h1>
-        <Button variant={"outline"}>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </Button>
+    <div className=" h-16 bg-slate-200 pl-5">
+      <div className="flex justify-between items-center align-middle ">
+        <h1 className=" font-extrabold text-zinc-500 text-2xl">DIPNET</h1>
+        <div className="flex justify-between items-center align-middle p-3 gap-5">
+          <Button variant={"outline"}>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Button>
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
